a5e1ede64f976f7692af2f4e50ba5cd4dfe6e1ce
hnani46 <22hr1a3246@mtieat.org>
2024-09-19 14:36:20 +0530

    Create odd elements.py

---
+++ odd elements.py
@@ -0,0 +1,13 @@
+....
+Write a Python Program to put the even and odd elements in a list into two different lists.
+....
+Sample Input:
+5
+1
+2
+3
+6
+5
+Sample Output:
+The even list [2, 6]
+The odd list [1, 3, 5]
