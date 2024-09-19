cee299fbe49c9bf23cfb1207bc858c2b10322be7
hnani46 <22hr1a3246@mtieat.org>
2024-09-19 14:40:36 +0530

    Create  search element.py

---
+++ search element.py
@@ -0,0 +1,15 @@
+....
+Write a Program to find the search element in the given list
+....
+Sample Input 1:
+5
+1 2 3 6 5
+3
+Sample Output 1:
+3 is present in the given list
+Sample Input 2:
+5
+1 2 3 6 5
+4
+Sample Output 2:
+4 is not present in the given list
